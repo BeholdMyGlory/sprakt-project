77941ecbe1a4a3b18cdfccf45e0e3176d5ddfcf3
Arvid Fahlström Myrman <behold@behold.se>
2015-12-08 13:49:35 +0100

    Changed tabs to spaces

--- common.py
+++ common.py
@@ -1,60 +1,60 @@
 def print_matrix(H):
-	m = len(H)-1
-	n = len(H[0])-1
+    m = len(H)-1
+    n = len(H[0])-1
 
-	l = max(len(str(max(max(H)))), len(str(min(min(H)))))
-	for j in range(n+1):
-		row = ""
-		for i in range(m+1):
-			row += " " + str(H[i][j]).rjust(l) + " "
-		print(row)
+    l = max(len(str(max(max(H)))), len(str(min(min(H)))))
+    for j in range(n+1):
+        row = ""
+        for i in range(m+1):
+            row += " " + str(H[i][j]).rjust(l) + " "
+        print(row)
 
 def find_one(dirs, a, b, fill="-"):
-	alignA = ""
-	alignB = ""
+    alignA = ""
+    alignB = ""
 
-	i = len(a)
-	j = len(b)
-	while i>0 or j>0:
-		if (i-1,j-1) in dirs[(i,j)]:
-			alignA = a[i-1] + alignA
-			alignB = b[j-1] + alignB
-			i -= 1
-			j -= 1
-		elif (i-1,j) in dirs[(i,j)]:
-			alignA = a[i-1] + alignA
-			alignB = fill + alignB
-			i -= 1
-		elif (i,j-1) in dirs[(i,j)]:
-			alignA = fill + alignA
-			alignB = b[j-1] + alignB
-			j -= 1
+    i = len(a)
+    j = len(b)
+    while i>0 or j>0:
+        if (i-1,j-1) in dirs[(i,j)]:
+            alignA = a[i-1] + alignA
+            alignB = b[j-1] + alignB
+            i -= 1
+            j -= 1
+        elif (i-1,j) in dirs[(i,j)]:
+            alignA = a[i-1] + alignA
+            alignB = fill + alignB
+            i -= 1
+        elif (i,j-1) in dirs[(i,j)]:
+            alignA = fill + alignA
+            alignB = b[j-1] + alignB
+            j -= 1
 
-	return (alignA, alignB)
+    return (alignA, alignB)
 
 def find_alignment(dirs, a, b, fill="-"):
-	if False:
-		A = find_all(dirs, a, b, fill, len(a), len(b))
-		print(A)
-		return A[0]
-	else:
-		return find_one(dirs, a, b, fill)
+    if False:
+        A = find_all(dirs, a, b, fill, len(a), len(b))
+        print(A)
+        return A[0]
+    else:
+        return find_one(dirs, a, b, fill)
 
 def find_all(dirs, a, b, fill, i, j):
-	if i>0 or j>0:
-		align = []
-		if (i-1,j-1) in dirs[(i,j)]:
-			A = find_all(dirs,a,b,fill,i-1,j-1)
-			for x,y in A:
-				align.append((x + a[i-1], y + b[j-1]))
-		if (i-1,j) in dirs[(i,j)]:
-			A = find_all(dirs,a,b,fill,i-1,j)
-			for x,y in A:
-				align.append((x + a[i-1], y + fill))
-		if (i,j-1) in dirs[(i,j)]:
-			A = find_all(dirs,a,b,fill,i,j-1)
-			for x,y in A:
-				align.append((x + fill, y + b[j-1]))
+    if i>0 or j>0:
+        align = []
+        if (i-1,j-1) in dirs[(i,j)]:
+            A = find_all(dirs,a,b,fill,i-1,j-1)
+            for x,y in A:
+                align.append((x + a[i-1], y + b[j-1]))
+        if (i-1,j) in dirs[(i,j)]:
+            A = find_all(dirs,a,b,fill,i-1,j)
+            for x,y in A:
+                align.append((x + a[i-1], y + fill))
+        if (i,j-1) in dirs[(i,j)]:
+            A = find_all(dirs,a,b,fill,i,j-1)
+            for x,y in A:
+                align.append((x + fill, y + b[j-1]))
 
-		return align
-	return [("", "")]
+        return align
+    return [("", "")]
--- needleman_wunsch.py
+++ needleman_wunsch.py
@@ -3,60 +3,60 @@ import re
 from common import *
 
 def similarity(a, b):
-	if a == b:
-		return +1
-	else:
-		return -1
+    if a == b:
+        return +1
+    else:
+        return -1
 
 def needleman_wunsch(a, b, s=similarity, d=-1, fill="-"):
-	#W = lambda i: -1
-	#d = -1
+    #W = lambda i: -1
+    #d = -1
 
-	m = len(a)
-	n = len(b)
+    m = len(a)
+    n = len(b)
 
-	F = [[0 for j in range(n+1)] for i in range(m+1)]
+    F = [[0 for j in range(n+1)] for i in range(m+1)]
 
-	for i in range(1,m+1):
-		F[i][0] = -i
-	for j in range(1,n+1):
-		F[0][j] = -j
+    for i in range(1,m+1):
+        F[i][0] = -i
+    for j in range(1,n+1):
+        F[0][j] = -j
 
-	dirs = {}
-	for i in range(1,m+1):
-		for j in range(1,n+1):
-			match = F[i-1][j-1] + s(a[i-1], b[j-1])
-			deletion = F[i-1][j] + d
-			insertion = F[i][j-1] + d
-			F[i][j] = max(match, insertion, deletion)
+    dirs = {}
+    for i in range(1,m+1):
+        for j in range(1,n+1):
+            match = F[i-1][j-1] + s(a[i-1], b[j-1])
+            deletion = F[i-1][j] + d
+            insertion = F[i][j-1] + d
+            F[i][j] = max(match, insertion, deletion)
 
-			dirs[(i,j)] = []
-			if F[i][j] == match:
-				dirs[(i,j)].append((i-1,j-1))
-			if F[i][j] == deletion:
-				dirs[(i,j)].append((i-1,j))
-			if F[i][j] == insertion:
-				dirs[(i,j)].append((i,j-1))
+            dirs[(i,j)] = []
+            if F[i][j] == match:
+                dirs[(i,j)].append((i-1,j-1))
+            if F[i][j] == deletion:
+                dirs[(i,j)].append((i-1,j))
+            if F[i][j] == insertion:
+                dirs[(i,j)].append((i,j-1))
 
-	#print_matrix(F)
-	#for x,y in dirs:
-	#	print(str((x,y)) + ": " + str(dirs[(x,y)]))
+    #print_matrix(F)
+    #for x,y in dirs:
+    #    print(str((x,y)) + ": " + str(dirs[(x,y)]))
 
-	return find_alignment(dirs, a, b, fill)
+    return find_alignment(dirs, a, b, fill)
 
 def main():
-	seq1 = "GCATGCU"
-	seq2 = "GATTACA"
+    seq1 = "GCATGCU"
+    seq2 = "GATTACA"
 
-	print("Sequence A:  %s" % seq1)
-	print("Sequence B:  %s" % seq2)
+    print("Sequence A:  %s" % seq1)
+    print("Sequence B:  %s" % seq2)
 
-	(alignA,alignB) = needleman_wunsch(seq1, seq2)
+    (alignA,alignB) = needleman_wunsch(seq1, seq2)
 
-	print()
-	print("Alignment A: %s" % alignA)
-	print("Alignment B: %s" % alignB)
-	print()
+    print()
+    print("Alignment A: %s" % alignA)
+    print("Alignment B: %s" % alignB)
+    print()
 
 if __name__ == "__main__":
-	main()
+    main()
