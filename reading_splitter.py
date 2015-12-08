e1523a86d339418fa9722462c9e16ab237d04a26
Arvid Fahlström Myrman <behold@behold.se>
2015-12-08 16:42:39 +0100

    Generate extra readings recursively

--- reading_splitter.py
+++ reading_splitter.py
@@ -6,15 +6,41 @@ conn = sqlite3.connect("kanjidic.db")
 
 def process_reading(reading):
     hiragana = "".join(chr(ord(c) - (ord('ァ') - ord('ぁ')))
-                      if "ァ" <= c <= "ヶ" else c
-                      for c in reading)
-    return hiragana.split(".")[0].strip("-")
+                       if "ァ" <= c <= "ヶ" else c
+                       for c in reading)
+    hiragana = hiragana.split(".")[0].strip("-")
+    generated_readings = {hiragana}
+
+    def generate_extra_readings(hiragana):
+        # handle sokuon
+        if hiragana[-1] in "きくちつ":
+            yield hiragana[:-1] + "っ"
+        # handle rendaku
+        if hiragana[0] in "かきくけこさしすせそたちつてとはひふへほ":
+            yield chr(ord(hiragana[0]) + 1) + hiragana[1:]
+        # handle handakuten for ha row
+        if hiragana[0] in "はひふへほ":
+            yield chr(ord(hiragana[0]) + 2) + hiragana[1:]
+        # handle homophones for dzi, dzu
+        if hiragana[0] in "ちつ":
+            yield {"ち": "じ", "つ": "ず"}[hiragana[0]] + hiragana[1:]
+
+    old = set()
+
+    while generated_readings != old:
+        old = generated_readings
+        generated_readings = generated_readings | {new_reading
+                                                   for old_reading in generated_readings
+                                                   for new_reading in generate_extra_readings(old_reading)}
+
+    return generated_readings
 
 def get_readings(kanji):
     c = conn.cursor()
-    readings =  {process_reading(reading)
+    readings =  {processed_reading
                  for reading, reading_type in c.execute(
-                     "SELECT reading, type from readings where kanji=?", (kanji,))}
+                     "SELECT reading, type from readings where kanji=?", (kanji,))
+                 for processed_reading in process_reading(reading)}
     return readings
 
 def split_reading(kanji, kana, skip=False):
@@ -43,4 +69,5 @@ def split_reading(kanji, kana, skip=False):
 
     if minScore > 0 and skip:
         return [(kanji, kana)]
+
     return result
